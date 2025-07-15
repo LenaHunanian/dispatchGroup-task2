d0f6c1ec6c3ea75e1d04ecef25437a84ab97be0d
lena hunanian <lenasss20053112@gmail.com>
2025-07-15 16:56:11 +0400

    first commit

---
+++ Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+import Foundation
+
+let group = DispatchGroup()
+
+func executeTask(_ taskNumber: Int, delay: UInt32) {
+    print("Task \(taskNumber) started")
+    sleep(delay) // Simulate work
+    print("Task \(taskNumber) finished")
+}
+
+func executeTasks() {
+    let queue1 = DispatchQueue(label: "com.example.queue1", attributes: .concurrent)
+    let queue2 = DispatchQueue(label: "com.example.queue2", attributes: .concurrent)
+    let queueFinal = DispatchQueue(label: "com.example.finalQueue")
+    
+    
+    group.enter()
+    queue1.async {
+        executeTask(1, delay: 2)
+        group.leave()
+    }
+    group.enter()
+    queue2.async {
+        
+        executeTask(2, delay: 3)
+        group.leave()
+    }
+    
+    group.notify(queue: .main){
+        queueFinal.async {
+            executeTask(3, delay: 1)
+        }
+    }
+}
+
+// Run the function
+executeTasks()
+
